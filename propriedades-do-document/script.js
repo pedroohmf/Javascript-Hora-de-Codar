017a682801c24932c518cfab7e9a986380779609
Pedro Henrique <pedrohfm2012@yahoo.com.br>
2022-09-22 16:50:35 -0300

    Propriedades do document

---
+++ propriedades-do-document/script.js
@@ -0,0 +1,13 @@
+// propriedades document
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links[0]); // todos os links da pagina
+
+document.links[0].textContent = "Instagram";
+
+console.log(document.URL);
+
+console.log(document.title);
