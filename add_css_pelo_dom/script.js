df89d6f495735fce0d2b50e02cf5a5cf67af7719
Pedro Henrique <pedrohfm2012@yahoo.com.br>
2022-09-22 16:40:08 -0300

    Adicionando CSS pelo DOM

---
+++ add_css_pelo_dom/script.js
@@ -0,0 +1,10 @@
+let title = document.querySelector("#title");
+
+title.style.color = "red";
+title.style.backgroundColor = "green";
+
+
+// selecionar elemento subtitle
+let subtitle = document.querySelector(".subtitle");
+
+subtitle.style.cssText = "color: blue; font-size: 20px; font-weight: bold;";
