fa420afcb0e16e5f4e169fbfa01b1d5692f8c9b0
Pedro Henrique <pedrohfm2012@yahoo.com.br>
2022-09-25 20:24:14 -0300

    Eventos e onload

---
+++ eventos_e_onload/script.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    console.log("carregou o DOM");
+    var title2 = document.querySelector("#title");
+    console.log(title2);
+}
+
+console.log("Carregou o JS");
+
+let title = document.querySelector("#title");
+
+console.log(title);
